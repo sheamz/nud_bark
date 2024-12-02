aeb8fbaf69b7013facd89c4e46c0c38de25e9bfc
redthecoderist1000 <redochavillo@gmail.com>
2024-12-02 23:54:12 +0800

    login-register

--- src/pages/login-signin/Login.jsx
+++ src/pages/login-signin/Login.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import "./Login.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Link, useNavigate } from "react-router-dom";
 // import Button from "@mui/material/Button";
 
 export default function Login() {
+  const navigate = useNavigate();
+  let SubmitForm = (e) => {
+    e.preventDefault();
+    navigate("/home");
+  };
+
   return (
     <div className="login_bg">
       <div className="left">
-        <Form>
-          <h1>Sign In</h1>
-          {/* <Button className="text-black" variant="outline-secondary">
-            Sign up with Google
-          </Button>
-          <Form.Text>or use your account</Form.Text> */}
+        <Form onSubmit={SubmitForm}>
+          <h1>Log In</h1>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter" />
+            <Form.Control type="email" placeholder="Email" required />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" required />
           </Form.Group>
           <Button variant="primary" className="btn_submit" type="submit">
-            Submit
+            Log In
           </Button>
+          <p className="mt-3">
+            Not registered yet? Register <Link to={"register"}>here</Link>
+          </p>
         </Form>
       </div>
       <div className="right"></div>
