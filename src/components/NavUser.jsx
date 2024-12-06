0b208c8e99ca90540e9e7af6933f19349362ae13
redthecoderist1000 <redochavillo@gmail.com>
2024-12-06 12:08:57 +0800

    user-nav profile

--- src/components/NavUser.jsx
+++ src/components/NavUser.jsx
@@ -2,37 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./NavUser.css";
 import Logo from "../assets/logo.png";
-import { Stack, Avatar, Divider } from "@mui/material";
+import { Stack, Avatar, Divider, Collapse } from "@mui/material";
 
 export default function NavUser() {
   return (
-    <div className="nav_bar d-flex sticky-top px-3">
-      <Stack direction="row" gap={2} sx={{ alignItems: "center" }}>
-        <img src={Logo} alt="" style={{ width: "56px", height: "56px" }} />
-        <h1>BARK</h1>
+    <div className="nav_bar d-flex justify-content-between sticky-top px-3">
+      <Stack direction="row" alignItems={"center"} width={"50%"}>
+        <Stack direction="row" gap={2} alignItems={"center"}>
+          <img
+            src={Logo}
+            alt="Bark Logo"
+            style={{ width: "56px", height: "56px" }}
+          />
+          <h1>BARK</h1>
+        </Stack>
+        <Divider
+          orientation="vertical"
+          flexItem
+          variant="middle"
+          sx={{
+            mx: "20px",
+            border: "1px solid #4554a6",
+            borderRadius: "5px",
+          }}
+        />
+        <Stack direction="row">
+          <ul>
+            <Link to={"/home"}>
+              <li>Home</li>
+            </Link>
+            <Link to={"/home"}>
+              <li>Browse</li>
+            </Link>
+            <Link to={"/create-topic"}>
+              <li>Create</li>
+            </Link>
+          </ul>
+        </Stack>
       </Stack>
-      <Divider
-        orientation="vertical"
-        flexItem
-        variant="middle"
-        sx={{
-          mx: "20px",
-          border: "1px solid #4554a6",
-          borderRadius: "5px",
-        }}
-      />
-      <Stack direction="row">
-        <ul>
-          <Link to={"/home"}>
-            <li>Home</li>
-          </Link>
-          <Link to={"/home"}>
-            <li>Browse</li>
-          </Link>
-          <Link to={"/create-topic"}>
-            <li>Create</li>
-          </Link>
-        </ul>
+      <Stack direction="row" alignItems={"center"} gap={1} mr={4}>
+        <Avatar>r</Avatar>
+        <p className="username">uusername</p>
+        <Collapse in={open} timeout="auto" unmountOnExit></Collapse>
       </Stack>
     </div>
   );
